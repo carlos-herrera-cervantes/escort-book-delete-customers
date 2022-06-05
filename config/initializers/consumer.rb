ec49009ad6287aa59d8b940bef6b00089eb13faf
Carlos Herrera <43161662+carlos-herrera-cervantes@users.noreply.github.com>
2022-06-05 10:30:56 -0500

    EB-113 Cancel user deletion when login

--- config/initializers/consumer.rb
+++ config/initializers/consumer.rb
@@ -5,13 +5,17 @@ require_relative '../../app/repositories/scheduled_removal_repository'
 
 require 'json'
 
-kafka_service = KafkaService.new
-kafka_service.consumer.subscribe('user-delete-account', default_offset: :earliest)
+delete_user_consumer = KafkaService.new
+delete_user_consumer.consumer.subscribe('user-delete-account', default_offset: :earliest)
+
+cancel_delete_consumer =  KafkaService.new
+cancel_delete_consumer.consumer.subscribe('user-active-account', default_offset: :earliest)
+
 repository = ScheduledRemovalRepository.new
 
 Thread.new do
   loop do
-    kafka_service.consumer.each_message do |message|
+    delete_user_consumer.consumer.each_message do |message|
       record_value = message.value
       parsed_record = JSON.parse record_value
 
@@ -26,9 +30,26 @@ Thread.new do
 
       repository.create hash
 
-      p 'MESSAGE PROCESSED'
+      p "SCHEDULED REMOVAL OF ACCOUNT FOR USER: #{hash['user_email']}"
     rescue Kafka::Error => e
-      p "ERROR PROCESSING A MESSAGE: #{e.message}"
+      p "ERROR SCHEDULING REMOVAL OF ACCOUNT: #{e.message}"
+    end
+  end
+end
+
+Thread.new do
+  loop do
+    cancel_delete_consumer.consumer.each_message do |message|
+      record_value = message.value
+      parsed_record = JSON.parse record_value
+
+      bson_user_id = BSON::ObjectId.from_string(parsed_record['userId'])
+      filter = { user_id: bson_user_id }
+      repository.destroy_all_by_filter filter
+
+      p "CANCEL DELETION OF ACCOUNT FOR USER: #{parsed_record['userId']}"
+    rescue Exception => e
+      p "ERROR CANCELING DELETION OF ACCOUNT: #{e.message}"
     end
   end
 end
